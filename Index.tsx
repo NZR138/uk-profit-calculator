e626a4d0495e0a5e443eef2960482a352bd218de
NZR138 <nzrbus@gmail.com>
2026-01-20 09:43:17 +0000

    Add files via upload

---
+++ Index.tsx
@@ -0,0 +1,72 @@
+import { ProfitCalculator } from "@/components/ProfitCalculator";
+
+const Index = () => {
+  return <ProfitCalculator />;
+};
+
+export default Index;
+public;
+
+_redirects;
+
+favicon.ico;
+
+placeholder.svg;
+
+robots.txt;
+
+src;
+
+components;
+
+hooks;
+
+integrations / supabase;
+
+lib;
+
+pages;
+
+App.css;
+
+App.tsx;
+
+index.css;
+
+main.tsx;
+
+vite - env.d.ts;
+
+supabase;
+
+config.toml.env.gitignore;
+
+bun.lock;
+
+components.json;
+
+eslint.config.js;
+
+index.html;
+
+package.json;
+
+postcss.config.js;
+
+README.md;
+
+tailwind.config.ts;
+
+tsconfig.app.json;
+
+tsconfig.app.tsbuildinfo;
+
+tsconfig.json;
+
+tsconfig.node.json;
+
+tsconfig.node.tsbuildinfo;
+
+vercel.json;
+
+vite.config.ts;
